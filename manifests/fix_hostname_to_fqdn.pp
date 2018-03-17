46ac307f4251b80dd3c38892ddd3c4401f4db6f1
suhailc <info@siterollout.com>
2018-03-17 15:07:40 +0000

    lint fixes

--- manifests/fix_hostname_to_fqdn.pp
+++ manifests/fix_hostname_to_fqdn.pp
@@ -2,10 +2,10 @@
 # class to fix the hostname to fqdn value as it reverts back to shortname breaking mail for the default Cpanel mail account
 
 class cpanel::fix_hostname_to_fqdn (
-  $fqdn_hostname = "${fqdn}"
+  $fqdn_hostname = "${::fqdn}"
 ) {
 
-  exec { "fix_hostname_to_fqdn":
+  exec { 'fix_hostname_to_fqdn':
     command => "/usr/local/cpanel/bin/set_hostname ${fqdn_hostname}",
     unless  => "/usr/bin/hostname -eq ${fqdn_hostname}",
   }
--- manifests/install.pp
+++ manifests/install.pp
@@ -3,13 +3,13 @@
 
 class cpanel::install {
 
-  package { "perl": ensure => "installed"}
-	
-  exec { "install_cpanel":
-    cwd => "/home",
-    command => "/usr/bin/wget -N http://httpupdate.cpanel.net/latest && /bin/sh latest",
+  package { 'perl': ensure => 'installed'}
+
+  exec { 'install_cpanel':
+    cwd     => '/home',
+    command => '/usr/bin/wget -N http://httpupdate.cpanel.net/latest && /bin/sh latest',
     timeout => 0,
-    creates => "/usr/local/cpanel"
+    creates => '/usr/local/cpanel'
   }
 
 }
