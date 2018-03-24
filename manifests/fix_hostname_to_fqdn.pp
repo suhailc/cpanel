c2ae29d455be36748f315d9dee6868903e6a4522
suhailc <info@siterollout.com>
2018-03-24 10:29:12 +0000

    change hostname check logic

--- manifests/fix_hostname_to_fqdn.pp
+++ manifests/fix_hostname_to_fqdn.pp
@@ -2,12 +2,13 @@
 # class to fix the hostname to fqdn value as it reverts back to shortname breaking mail for the default Cpanel mail account
 
 class cpanel::fix_hostname_to_fqdn (
-  $fqdn_hostname = "${::fqdn}"
 ) {
 
+  requires cpanel::install
+
   exec { 'fix_hostname_to_fqdn':
-    command => "/usr/local/cpanel/bin/set_hostname ${fqdn_hostname}",
-    unless  => "/usr/bin/hostname -eq ${fqdn_hostname}",
+    command => "/usr/local/cpanel/bin/set_hostname ${::fqdn}",
+    unless  => "/usr/bin/hostname -eq ${::fqdn}",
   }
 
 }
